6c8f1c6df519ca3a1de04fe8d6d6597f3d0f1246
jgsuddock <jacob.suddock@gmail.com>
2015-03-20 13:05:00 -0500

    Transposition
    Matrix Transposition Added

--- jake.py
+++ jake.py
@@ -1,1 +1,12 @@
 
+matrix = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+
+matrix = [[1,2,3],[4,5,6]]
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+transposed = [[0 for x in range(len(matrix))] for x in range(len(matrix[0]))]
+
+for t in range(0, len(matrix)):
+	for tt in range(0, len(matrix[t])):
+		transposed[tt][t] = matrix[t][tt]
